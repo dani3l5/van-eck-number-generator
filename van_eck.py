5e06b7df6ab094b6a3dfcd8fccfcd0c4a45d823b
dani3l5 <daniel.nicholson04@outlook.com>
2019-06-10 20:15:04 +0100

    Added the python file

---
+++ van_eck.py
@@ -0,0 +1,16 @@
+import sys
+seq = [0, 0]
+seq_len = int(sys.argv[1])
+for i in range(2, seq_len):
+	if seq[-1] in seq[:-1]:
+		indices = [i for i, x in enumerate(seq) if x == seq[-1]]
+		largest_index = indices[-2]
+		dist = (len(seq) - 1) - largest_index
+		seq.append(dist)
+	else:
+		seq.append(0)
+
+van_eck_index = 0
+for n in seq:
+	van_eck_index += 1
+	print(str(van_eck_index) + ": " + str(n))
